b09611405b9009baa9b675157c1d5738a69d45fd
ronaldbuchwald <37968557+ronaldbuchwald@users.noreply.github.com>
2025-02-26 11:30:17 -0500

    Add files via upload

---
+++ NOT-PH_test/NOT-PH_test.ino
@@ -0,0 +1,20 @@
+/*
+just to test if it reads anything.*/
+
+int pH_Value; 
+float Voltage;
+ 
+void setup() 
+{ 
+  Serial.begin(115200
+  );
+  pinMode(pH_Value, INPUT); 
+} 
+ 
+void loop() 
+{ 
+  pH_Value = analogRead(A0); 
+  Voltage = pH_Value * (5.0 / 1023.0); 
+  Serial.println(Voltage); 
+  delay(500); 
+}
