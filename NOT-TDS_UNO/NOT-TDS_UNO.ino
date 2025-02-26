9539fe4a487ea0345f9abdc21111dfb1dd20b5a9
ronaldbuchwald <37968557+ronaldbuchwald@users.noreply.github.com>
2025-02-26 10:42:10 -0500

    Add files via upload

---
+++ NOT-TDS_UNO/NOT-TDS_UNO.ino
@@ -0,0 +1,40 @@
+/***************************************************
+ This is the TDS sensor code IT needs to be ported to an ESP 32 Heltec V3 board
+ It is now runnig on an arduino UNO BOARD.
+ ****************************************************/
+ 
+ /***********Notice and Trouble shooting***************
+ 1. This code is tested on Arduino Uno with Arduino IDE 1.0.5 r2 and 1.8.2.
+ 2. Calibration CMD:
+     enter -> enter the calibration mode
+     cal:tds value -> calibrate with the known tds value(25^c). e.g.cal:707
+     exit -> save the parameters and exit the calibration mode
+ ****************************************************/
+
+#include <EEPROM.h>
+#include "GravityTDS.h"
+
+#define TdsSensorPin A1
+GravityTDS gravityTds;
+
+float temperature = 25,tdsValue = 0;
+
+void setup()
+{
+    Serial.begin(115200);
+    gravityTds.setPin(TdsSensorPin);
+    gravityTds.setAref(5.0);  //reference voltage on ADC, default 5.0V on Arduino UNO
+    gravityTds.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
+    gravityTds.begin();  //initialization
+}
+
+void loop()
+{
+    //temperature = readTemperature();  //add your temperature sensor and read it
+    gravityTds.setTemperature(temperature);  // set the temperature and execute temperature compensation
+    gravityTds.update();  //sample and calculate 
+    tdsValue = gravityTds.getTdsValue();  // then get the value
+    Serial.print(tdsValue,0);
+    Serial.println("ppm");
+    delay(1000);
+}
